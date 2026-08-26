6e5d6dee2d196f945755cd232e8971ce93827a7f
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2026-08-26 21:34:54 +0530

    Feat: Add adapter & page title

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
@@ -11,7 +11,7 @@ export default defineConfig({
           filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
       },
 
-      adapter: adapter(),
+      adapter: adapter({ fallback: "index.html" }),
     }),
   ],
   optimizeDeps: {
